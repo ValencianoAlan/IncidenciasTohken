aafba25eab2ef57906f0780099a67424d91eaa87
Alan Valenciano <valenciano.alan145@outlook.com>
2025-02-21 13:45:43 -0600

    Programa Funcionando con BD Prueba_3

--- Original/app/models/user_model.py
+++ Original/app/models/user_model.py
@@ -8,7 +8,7 @@ class UserModel:
         self.connection_string = (
             "DRIVER={ODBC Driver 17 for SQL Server};"
             "SERVER=localhost;"
-            "DATABASE=Prueba_3;"
+            "DATABASE=Prueba_4;"
             "UID=sa;"
             "PWD=root"
         )
